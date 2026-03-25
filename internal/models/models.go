d72ae7e6b5b44986896e2647c7f1f03305834429
samanvithiru <samanvithiru@gmail.com>
2026-03-24 20:17:50 -0400

    Basic structs for oh request and profiles

---
+++ internal/models/models.go
@@ -0,0 +1,16 @@
+package models
+
+// Keeping editing basic structure
+type OHRequest struct {
+	ComputingID string
+	Reason      string
+	DateTime    string
+}
+
+// Keep editing basic structure
+type Profile struct {
+	Name      string
+	Title     string
+	Resources string
+	Bio       string
+}
