4e9bed471707d6ce9e073b12618bf85b7d0ff19f
samanvithiru <samanvithiru@gmail.com>
2026-04-02 10:33:48 -0400

    Small tweaks to allow code to run

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,23 +1,41 @@
 package main
 
-//the starting point for the project for everything
-
 import (
 	"fmt"
 	"log"
 	"net/http"
-	//"html/template"
+	"notif/internal/data"   // Your data package
+	"notif/internal/models" // Your models package
 )
 
-// example handler
-func examplehandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "booltest %s!", r.URL.Path[1:])
+// signupHandler: Aligned with your existing models.OHRequest
+func signupHandler(w http.ResponseWriter, r *http.Request) {
+	// Using the exact field names from your models: ComputingID, TAID, DateTime, Reason
+	newReq := models.OHRequest{
+		ComputingID: "mst3k", // The Student
+		TAID:        "samanvi_01",
+		DateTime:    "2026-03-27 14:00",
+		Reason:      "Help with Excel integration",
+	}
+
+	// Calling your existing SaveOHRequest function
+	// We pass the filename we want to save to
+	err := data.SaveOHRequest("OH_Requests.xlsx", newReq)
+	if err != nil {
+		log.Printf("Error saving to Excel: %v", err)
+		http.Error(w, "Failed to save request", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "Successfully booked OH for %s with TA %s!", newReq.ComputingID, newReq.TAID)
 }
 
-// run handler through this when we need to check for certain certification before request can be handled
+func examplehandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the OH System at %s!", r.URL.Path[1:])
+}
+
 func certificationRequired(next http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		//rudimentary check currently, we need to add in specifics
 		if r.TLS != nil {
 			next.ServeHTTP(w, r)
 		} else {
@@ -27,14 +45,21 @@ func certificationRequired(next http.Handler) http.HandlerFunc {
 }
 
 func main() {
-	//when making a new handler, make sure to add it here accordingly
+	// Standard Endpoints
 	http.HandleFunc("/", examplehandler)
+
+	// New Excel Integration Endpoint
+	http.HandleFunc("/signup", signupHandler)
+
+	// Secure Endpoint
 	http.HandleFunc("/secure", certificationRequired(http.HandlerFunc(examplehandler)))
 
-	//certificate and key file paths, make sure the files are actually here
 	certFile := "cert.crt"
 	keyFile := "private.key"
 
+	fmt.Println("Server starting on https://localhost:8080...")
+
+	// Reminder: Ensure cert.crt and private.key are in your project folder
 	err := http.ListenAndServeTLS(":8080", certFile, keyFile, nil)
 	if err != nil {
 		log.Fatalf("HTTPS server failed: %v", err)
